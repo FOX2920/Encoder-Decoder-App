fd83b23a134eec3e6154cac5f826e60a9c64c318
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-15 10:35:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import re
+from collections import Counter
 
 def caesar_cipher(text, key, decrypt=False):
     result = ""
@@ -15,22 +17,48 @@ def brute_force_decrypt(ciphertext):
         decrypted_text = caesar_cipher(ciphertext, key, decrypt=True)
         st.write(f"Key {key}: {decrypted_text}")
 
+def analyze_frequency(ciphertext):
+    ciphertext = re.sub(r'[^a-zA-Z]', '', ciphertext)
+    frequencies = Counter(ciphertext)
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+    return sorted_frequencies
+
+def decrypt_ciphertext(ciphertext, mapping):
+    decrypted_text = ''.join(mapping.get(char, char) for char in ciphertext)
+    return decrypted_text
+
 def main():
-    st.title("Caesar Cipher Encryption/Decryption")
+    st.title("Cipher Decryption with Streamlit")
 
-    operation = st.sidebar.radio("Choose Operation", ["Encrypt", "Decrypt", "Brute Force Decrypt"])
+    operation = st.sidebar.radio("Choose Operation", ["Caesar Cipher", "Brute Force Decrypt"])
 
-    key = st.sidebar.number_input("Enter Key", min_value=0, max_value=25, value=3)
+    if operation == "Caesar Cipher":
+        key = st.sidebar.number_input("Enter Key", min_value=0, max_value=25, value=3)
+        text = st.text_area(f"Enter {'Plaintext' if key > 0 else 'Ciphertext'}", "")
+        result = caesar_cipher(text, key, decrypt=(key > 0))
+        st.write(f"Result: {result}")
 
-    if operation == "Brute Force Decrypt":
+    elif operation == "Brute Force Decrypt":
         ciphertext = st.text_area("Enter Ciphertext", "")
         if st.button("Brute Force Decrypt"):
             brute_force_decrypt(ciphertext)
-    else:
-        text = st.text_area(f"Enter {'Plaintext' if operation=='Encrypt' else 'Ciphertext'}", "")
-        result = caesar_cipher(text, key, decrypt=(operation == "Decrypt"))
 
-        st.write(f"Result: {result}")
+            # Perform frequency analysis
+            st.write("Frequency Analysis:")
+            symbol_frequencies = analyze_frequency(ciphertext)
+            st.write(symbol_frequencies)
+
+            # English letter frequencies for reference
+            english_frequencies = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
+
+            # Initial mapping (replace this with your guesses)
+            mapping = dict(zip([sym for sym, _ in symbol_frequencies], english_frequencies))
+
+            # Decrypt the text
+            decrypted_text = decrypt_ciphertext(ciphertext, mapping)
+
+            st.write("\nDecrypted Text:")
+            st.write(decrypted_text)
 
 if __name__ == "__main__":
     main()
