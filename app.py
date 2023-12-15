476096c109e2088865295299b715302409821cf1
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-15 11:53:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ def caesar_cipher(text, key, decrypt=False):
             result += char
     return result
 
-def brute_force_decrypt(ciphertext):
+def brute_force(ciphertext, mapping=None):
+    results = []
     for key in range(26):
         decrypted_text = caesar_cipher(ciphertext, key, decrypt=True)
-        st.write(f"Key {key}: {decrypted_text}")
+        if mapping:
+            decrypted_text = apply_mapping(decrypted_text, mapping)
+        results.append(f"Key {key}: {decrypted_text}")
+    return results
 
-def brute_force_encrypt(ciphertext):
-    for key in range(26):
-        decrypted_text = caesar_cipher(ciphertext, key, decrypt=False)
-        st.write(f"Key {key}: {decrypted_text}")
-
+def apply_mapping(text, mapping):
+    return ''.join(mapping.get(char, char) for char in text)
 
 def main():
     st.title("Caesar Cipher Encryption/Decryption")
@@ -28,22 +29,26 @@ def main():
 
     key = st.sidebar.number_input("Enter Key", min_value=0, max_value=25, value=3)
 
-    if operation == "Brute Force Decrypt":
+    # Given mapping for decryption
+    mapping = {
+        '5': 'a', '2': 'b', '-': 'c', '†': 'd', '8': 'e', '1': 'f', '3': 'g', '4': 'h',
+        '6': 'i', '0': 'j', '9': 'k', '*': 'l', '‡': 'm', '.': 'n', '$': 'o',
+        '(': 'p', ')': 'q', ';': 'r', '?': 's', '¶': 't', ']': 'u', '¢': 'v', ':': 'w',
+        '[': 'x', ':': 'y', '[': 'z'
+    }
+
+    if operation == "Brute Force Decrypt" or operation == "Brute Force Encrypt":
         ciphertext = st.text_area("Enter Ciphertext", "")
-        if st.button("Brute Force Decrypt"):
-            brute_force_decrypt(ciphertext)
-    elif operation == "Brute Force Decrypt":
-        ciphertext = st.text_area("Enter Ciphertext", "")
-        if st.button("Brute Force Encrypt"):
-            brute_force_encrypt(ciphertext)
-    elif operation == "Decrypt":
-        ciphertext = st.text_area("Enter Ciphertext", "")
-        decrypted_text = caesar_cipher(ciphertext, key, decrypt=True)
-        st.write(f"Decrypted Text: {decrypted_text}")
+        if st.button(f"Brute Force {operation.split(' ')[-1]}"):
+            results = brute_force(ciphertext, mapping if operation == "Brute Force Decrypt" else None)
+            for result in results:
+                st.write(result)
     else:
         text = st.text_area(f"Enter {'Plaintext' if operation=='Encrypt' else 'Ciphertext'}", "")
         result = caesar_cipher(text, key, decrypt=(operation == "Decrypt"))
-        st.write(f"Result: {result}")
+        result_with_mapping = apply_mapping(result, mapping)
+
+        st.write(f"Result: {result_with_mapping}")
 
 if __name__ == "__main__":
     main()
