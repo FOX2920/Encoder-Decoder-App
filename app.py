747d8f4088c626773f1500686f64e04d8e6276da
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 19:51:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from pages import Caesar, Playfair, Vigenere, XOR_cipher, Capture_the_flag
+# Chọn ứng dụng
+selected_app = st.sidebar.selectbox("Chọn ứng dụng", ["Caesar", "Playfair", "Vigenere", "XOR_cipher", "Capture_the_flag"])
 
+# Hiển thị ứng dụng được chọn
+if selected_app == "Caesar":
+    Caesar.main()
+elif selected_app == "Playfair":
+    Playfair.main()
+elif selected_app == "Vigenere":
+    Vigenere.main()
+elif selected_app == "Capture_the_flag":
+    Capture_the_flag.main()
+else:
+    Caesar.main()
+  
