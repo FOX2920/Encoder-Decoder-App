cb0d8f17bd0246712b20d0dc9a7b0b4537bdcdb6
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 21:05:44 +0700

    Update XOR_cipher.py

--- pages/XOR_cipher.py
+++ pages/XOR_cipher.py
@@ -1,33 +1,43 @@
 import streamlit as st
+import io
+from PIL import Image
 
-def xor_decrypt(encrypted_data, key):
-    decrypted_data = bytearray()
-    key = key.encode()
+# XOR decryption function
+def decrypt(ciphertext, key):
+    decrypted = bytearray()
+    key_len = len(key)
+    for i, byte in enumerate(ciphertext):
+        decrypted_byte = byte ^ key[i % key_len]
+        decrypted.append(decrypted_byte)
+    return bytes(decrypted)
 
-    for i in range(len(encrypted_data)):
-        decrypted_byte = encrypted_data[i] ^ key[i % len(key)]
-        decrypted_data.append(decrypted_byte)
-
-    return bytes(decrypted_data)
-
+# Streamlit app
 def main():
-    st.title("XOR Cipher Decryption App")
+    st.title("Image Decryption App")
 
-    uploaded_file = st.file_uploader("Choose an encrypted image file", type=["jpg", "jpeg"])
+    # Upload image
+    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg"])
 
     if uploaded_file is not None:
-        key = st.text_input("Enter 6-letter key:")
-        if len(key) != 6:
-            st.warning("Please enter a 6-letter key.")
+        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+
+        # Input decryption key
+        decryption_key = st.text_input("Enter 6-letter decryption key:")
+
+        if len(decryption_key) == 6:
+            # Read image file
+            image_bytes = io.BytesIO(uploaded_file.read())
+            image = Image.open(image_bytes)
+
+            # Decrypt image
+            encrypted_data = image.tobytes()
+            decrypted_data = decrypt(encrypted_data, decryption_key.encode())
+
+            # Display decrypted image
+            decrypted_image = Image.frombytes(image.mode, image.size, decrypted_data)
+            st.image(decrypted_image, caption="Decrypted Image.", use_column_width=True)
         else:
-            with open(uploaded_file.name, "rb") as file:
-                encrypted_data = file.read()
-
-            decrypted_data = xor_decrypt(encrypted_data, key)
-
-            st.image(decrypted_data, caption="Decrypted Image", use_column_width=True)
-
-            st.success("Decryption successful! Check the image for the flag.")
+            st.warning("Please enter a 6-letter decryption key.")
 
 if __name__ == "__main__":
     main()
