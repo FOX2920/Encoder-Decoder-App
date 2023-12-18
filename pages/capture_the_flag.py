bf689eca044192e90b570d556aa0d465a95f4adb
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 17:41:43 +0700

    Create capture_the_flag.py

---
+++ pages/capture_the_flag.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+def ascii_to_text(ascii_text):
+    try:
+        text = ''.join(chr(int(code)) for code in ascii_text.split())
+        return text
+    except ValueError:
+        return "Invalid ASCII codes"
+
+def text_to_ascii(text):
+    return ' '.join(str(ord(char)) for char in text)
+
+def main():
+    st.title("ASCII Encoder and Decoder")
+    st.markdown(task_description)
+
+    operation = st.radio("Select Operation:", ["Decode ASCII to Text", "Encode Text to ASCII"])
+
+    if operation == "Decode ASCII to Text":
+        ascii_input = st.text_area("Enter ASCII Codes:")
+        if st.button("Decode"):
+            decoded_text = ascii_to_text(ascii_input)
+            st.text(f"Decoded Text: {decoded_text}")
+
+    elif operation == "Encode Text to ASCII":
+        text_input = st.text_area("Enter Text:")
+        if st.button("Encode"):
+            encoded_ascii = text_to_ascii(text_input)
+            st.text(f"Encoded ASCII Codes: {encoded_ascii}")
+
+if __name__ == "__main__":
+    main()
