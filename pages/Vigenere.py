f7b94f7cf49b83b71067ccef6ac7cc04dc12b8b8
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 19:21:48 +0700

    Update Vigenere.py

--- pages/Vigenere.py
+++ pages/Vigenere.py
@@ -1,55 +1,58 @@
 import streamlit as st
 
-
-def extend_key(key, length):
-    extended_key = key
-    while len(extended_key) < length:
-        extended_key += key
-    return extended_key[:length]
-
-
 def vigenere_encrypt(plain_text, key):
     encrypted_text = ""
-    key = extend_key(key.upper(), len(plain_text))
-    for i in range(len(plain_text)):
-        if plain_text[i].isalpha():
-            shift = ord(key[i]) - ord('A')
-            if plain_text[i].islower():
-                encrypted_text += chr((ord(plain_text[i]) + shift - ord('a')) % 26 + ord('a'))
-            else:
-                encrypted_text += chr((ord(plain_text[i]) + shift - ord('A')) % 26 + ord('A'))
-        else:
-            encrypted_text += plain_text[i]
-    return encrypted_text
+    key = key.upper()
+    key_index = 0
 
+    for char in plain_text:
+        if char.isalpha():
+            key_char = key[key_index % len(key)]
+            shift = ord(key_char) - ord('A')
+            if char.isupper():
+                encrypted_text += chr((ord(char) + shift - ord('A')) % 26 + ord('A'))
+            else:
+                encrypted_text += chr((ord(char) + shift - ord('a')) % 26 + ord('a'))
+            key_index += 1
+        else:
+            encrypted_text += char
+
+    return encrypted_text
 
 def vigenere_decrypt(encrypted_text, key):
     decrypted_text = ""
-    key = extend_key(key.upper(), len(encrypted_text))
-    for i in range(len(encrypted_text)):
-        if encrypted_text[i].isalpha():
-            shift = ord(key[i]) - ord('A')
-            if encrypted_text[i].islower():
-                decrypted_text += chr((ord(encrypted_text[i]) - shift - ord('a')) % 26 + ord('a'))
+    key = key.upper()
+    key_index = 0
+
+    for char in encrypted_text:
+        if char.isalpha():
+            key_char = key[key_index % len(key)]
+            shift = ord(key_char) - ord('A')
+            if char.isupper():
+                decrypted_text += chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
             else:
-                decrypted_text += chr((ord(encrypted_text[i]) - shift - ord('A')) % 26 + ord('A'))
+                decrypted_text += chr((ord(char) - shift - ord('a')) % 26 + ord('a'))
+            key_index += 1
         else:
-            decrypted_text += encrypted_text[i]
+            decrypted_text += char
+
     return decrypted_text
 
+# Streamlit UI
+st.title("Vigenère Cipher Encryption and Decryption")
 
-def vigenere_page():
-    st.title("Vigenère Cipher Encryption/Decryption")
+# Input text
+text = st.text_area("Enter your message:")
 
-    option = st.radio("Select an option:", ("Encrypt", "Decrypt"))
+# Input key
+key = st.text_input("Enter the encryption/decryption key:")
 
-    key = st.text_input("Enter the key:")
-    text = st.text_input("Enter the text:")
+# Encryption button
+if st.button("Encrypt"):
+    encrypted_result = vigenere_encrypt(text, key)
+    st.success(f"Encrypted Message: {encrypted_result}")
 
-    if key and text:
-        if option == "Encrypt":
-            result = vigenere_encrypt(text, key)
-            st.success(f"Encrypted Text: {result}")
-        else:
-            result = vigenere_decrypt(text, key)
-            st.success(f"Decrypted Text: {result}")
+# Decryption button
+if st.button("Decrypt"):
+    decrypted_result = vigenere_decrypt(text, key)
+    st.success(f"Decrypted Message: {decrypted_result}")
