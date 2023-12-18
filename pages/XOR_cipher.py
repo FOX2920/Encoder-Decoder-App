35de014bee5cf50e642c6470ad8bb2a3f3e5b2cf
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 21:02:58 +0700

    Update XOR_cipher.py

--- pages/XOR_cipher.py
+++ pages/XOR_cipher.py
@@ -1,71 +1,33 @@
 import streamlit as st
-from PIL import Image
-import io
 
-def xor_decrypt(image_path, key):
-    try:
-        image = Image.open(image_path)
-        pixels = image.load()
-        width, height = image.size
+def xor_decrypt(encrypted_data, key):
+    decrypted_data = bytearray()
+    key = key.encode()
 
-        decrypted_data = bytearray()
-        for y in range(height):
-            for x in range(width):
-                r, g, b = pixels[x, y]
-                decrypted_data.append(r ^ key[x % len(key)])
+    for i in range(len(encrypted_data)):
+        decrypted_byte = encrypted_data[i] ^ key[i % len(key)]
+        decrypted_data.append(decrypted_byte)
 
-        decrypted_text = decrypted_data.decode('utf-8')
-        return decrypted_text
-    except Exception as e:
-        return f"Error: {str(e)}"
-
-
-def brute_force_decrypt(image_path):
-    results = {}
-    for key in range(256):  # Assuming key values are between 0 and 255
-        try:
-            decrypted_text = xor_decrypt(image_path, bytes([key] * 6))
-            results[key] = decrypted_text
-        except:
-            pass
-    return results
-
+    return bytes(decrypted_data)
 
 def main():
-    st.title("Image Decryption with XOR Cipher")
+    st.title("XOR Cipher Decryption App")
 
-    task_description = """
-    Upload an encrypted image (crypto01.jpg) and find the flag.
-    The image was encrypted with an XOR cipher using a 6-letter key.
-    """
-    st.markdown(task_description)
-
-    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg"])
+    uploaded_file = st.file_uploader("Choose an encrypted image file", type=["jpg", "jpeg"])
 
     if uploaded_file is not None:
-        key = st.text_input("Enter the 6-letter key:")
+        key = st.text_input("Enter 6-letter key:")
         if len(key) != 6:
             st.warning("Please enter a 6-letter key.")
         else:
-            if st.button("Decrypt Image"):
-                try:
-                    image_bytes = uploaded_file.read()
-                    image_path = io.BytesIO(image_bytes)
-                    decrypted_text = xor_decrypt(image_path, key.encode('utf-8'))
-                    st.text(f"Decrypted Flag: {decrypted_text}")
-                except Exception as e:
-                    st.error(f"Error decrypting image: {str(e)}")
+            with open(uploaded_file.name, "rb") as file:
+                encrypted_data = file.read()
 
-            if st.button("Brute-Force Decrypt"):
-                try:
-                    image_bytes = uploaded_file.read()
-                    image_path = io.BytesIO(image_bytes)
-                    brute_force_results = brute_force_decrypt(image_path)
-                    st.text("Brute-Force Decryption Results:")
-                    for possible_key, possible_flag in brute_force_results.items():
-                        st.text(f"Key: {possible_key}, Flag: {possible_flag}")
-                except Exception as e:
-                    st.error(f"Error during brute-force decryption: {str(e)}")
+            decrypted_data = xor_decrypt(encrypted_data, key)
+
+            st.image(decrypted_data, caption="Decrypted Image", use_column_width=True)
+
+            st.success("Decryption successful! Check the image for the flag.")
 
 if __name__ == "__main__":
     main()
