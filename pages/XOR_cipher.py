9420cbe530a94c153935e9fdff4f38b0a92f78ce
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2023-12-18 21:22:25 +0700

    Update XOR_cipher.py

--- pages/XOR_cipher.py
+++ pages/XOR_cipher.py
@@ -15,11 +15,10 @@ st.title("XOR Cipher Decryption App")
 uploaded_file = st.file_uploader("Upload an encrypted image", type=["jpg", "jpeg"])
 
 if uploaded_file is not None:
-    with open(uploaded_file, "rb") as file:
-        encrypted_data = file.read()
-    
+    encrypted_data = uploaded_file.read()  # Corrected line
+
     st.subheader("Encryption Key")
-    key = st.text_input("Enter the encryption key (6 letters):", "b'uithcm'")
+    key = st.text_input("Enter the encryption key (6 letters):", "uithcm").encode()  # Corrected line
 
     # Correct the function call to decrypt
     decrypted_data = decrypt(encrypted_data, key)
@@ -29,6 +28,6 @@ if uploaded_file is not None:
 
     # Provide option to download the decrypted image
     st.sidebar.markdown(
-        f"**Download Decrypted Image:** [decrypted_image_{key}.jpg](data:application/octet-stream;base64,{base64.b64encode(decrypted_data).decode()})",
+        f"**Download Decrypted Image:** [decrypted_image_{key.decode()}.jpg](data:application/octet-stream;base64,{base64.b64encode(decrypted_data).decode()})",
         unsafe_allow_html=True
     )
